2d9ee2645dd063c4047cfa9188b5a5c7a68da949
Raquel Thiessen <raquel.thiessen@invenia.ca>
2021-05-27 15:38:29 +0000

    Add general classes for parsing xls and xlsx using different packages

---
+++ inveniautils/xlsutil.py
@@ -0,0 +1,91 @@
+from io import BytesIO
+
+import openpyxl
+import xlrd  # type: ignore
+
+
+class Cell:
+    # OPENPYXL data_type mappings are found here:
+    # https://openpyxl.readthedocs.io/en/stable/_modules/openpyxl/cell/cell.html
+    EMPTY_CELL_OPENPYXL = openpyxl.cell.cell.TYPE_NULL
+    TEXT_CELL_OPENPYXL = openpyxl.cell.cell.TYPE_STRING
+    NUMBER_CELL_OPENPYXL = openpyxl.cell.cell.TYPE_NUMERIC
+
+    # XLRD ctype mappings are found here: https://pythonhosted.org/xlrd3/cell.html
+    EMPTY_CELL_XLRD = xlrd.XL_CELL_EMPTY
+    TEXT_CELL_XLRD = xlrd.XL_CELL_TEXT
+    NUMBER_CELL_XLRD = xlrd.XL_CELL_NUMBER
+
+    def __init__(self, is_xls: bool, cell):
+        self.is_xls = is_xls
+        self.cell = cell
+        self.ctype = self._get_cell_type()
+        self.value = self.cell.value
+        self._set_type_mappings()
+
+    def _get_cell_type(self):
+        return self.cell.ctype if self.is_xls else self.cell.data_type
+
+    def _set_type_mappings(self):
+        if self.is_xls:
+            self.EMPTY_CELL = self.EMPTY_CELL_XLRD
+            self.TEXT_CELL = self.TEXT_CELL_XLRD
+            self.NUMBER_CELL = self.NUMBER_CELL_XLRD
+        else:
+            self.EMPTY_CELL = self.EMPTY_CELL_OPENPYXL
+            self.TEXT_CELL = self.TEXT_CELL_OPENPYXL
+            self.NUMBER_CELL = self.NUMBER_CELL_OPENPYXL
+
+
+class Sheet:
+    def __init__(self, is_xls: bool, sheet):
+        self.is_xls = is_xls
+        self.sheet = sheet
+        self.nrows = self._set_num_rows()
+        self.name = self._set_name()
+
+    def _set_num_rows(self):
+        return self.sheet.nrows if self.is_xls else self.sheet.max_row
+
+    def _set_name(self):
+        return self.sheet.name if self.is_xls else self.sheet.title
+
+    def row(self, row_num: int):
+        # openpyxl starts indexing at 1
+        initial_row = (
+            self.sheet.row(row_num) if self.is_xls else self.sheet[row_num + 1]
+        )
+
+        return list(Cell(self.is_xls, item) for item in initial_row)
+
+    def row_values(self, row_num: int):
+        # openpyxl starts indexing at 1
+        return (
+            self.sheet.row_values(row_num)
+            if self.is_xls
+            else list(item.value for item in self.sheet[row_num + 1])
+        )
+
+
+class Workbook:
+    def __init__(self, filename: str, content):
+        self.is_xls = filename.endswith(".xls")
+        self.workbook = self._open_workbook(content)
+
+    def _open_workbook(self, content):
+        return (
+            xlrd.open_workbook(file_contents=content)
+            if self.is_xls
+            else openpyxl.load_workbook(BytesIO(content))
+        )
+
+    def sheet_names(self):
+        return self.workbook.sheet_names() if self.is_xls else self.workbook.sheetnames
+
+    def sheet_by_name(self, sheet_name: str):
+        sheet = (
+            self.workbook.sheet_by_name(sheet_name)
+            if self.is_xls
+            else self.workbook.get_sheet_by_name(sheet_name)
+        )
+        return Sheet(self.is_xls, sheet)
---
+++ tests/test_xls.py
@@ -0,0 +1,111 @@
+import unittest
+from pathlib import Path
+
+from inveniautils.xlsutil import Workbook
+import openpyxl
+import xlrd  # type: ignore
+
+xls_file_name = "Sample_XLS.xls"
+xlsx_file_name = "Sample_XLSX.xlsx"
+sample_xls_path = Path("tests") / "files" / xls_file_name
+sample_xlsx_path = Path("tests") / "files" / xlsx_file_name
+
+
+class TestXLSUtil(unittest.TestCase):
+    def setup_xls(self):
+        file = open(sample_xls_path, "rb")
+        content = file.read()
+        self.assertNotEqual(content, None)
+        return Workbook(xls_file_name, content)
+
+    def setup_xlsx(self):
+        file = open(sample_xlsx_path, "rb")
+        content = file.read()
+        self.assertNotEqual(content, None)
+        return Workbook(xlsx_file_name, content)
+
+    def test_xls_sheet_names(self):
+        workbook = self.setup_xls()
+        sheet_names = workbook.sheet_names()
+        self.assertEqual(len(sheet_names), 1)
+        self.assertEqual(sheet_names[0], "TestSheet")
+
+    def test_xlsx_sheet_names(self):
+        workbook = self.setup_xlsx()
+        sheet_names = workbook.sheet_names()
+        self.assertEqual(len(sheet_names), 1)
+        self.assertEqual(sheet_names[0], "TestSheet")
+
+    def test_xls_sheet_by_name(self):
+        workbook = self.setup_xls()
+        sheet = workbook.sheet_by_name("TestSheet")
+        self.assertEqual(sheet.name, "TestSheet")
+
+    def test_xlsx_sheet_by_name(self):
+        workbook = self.setup_xlsx()
+        sheet = workbook.sheet_by_name("TestSheet")
+        self.assertEqual(sheet.name, "TestSheet")
+
+    def test_xls_row(self):
+        expected_0 = ["number", "name", "value"]
+        expected_1 = [1, "A", 3]
+        workbook = self.setup_xls()
+        sheet = workbook.sheet_by_name("TestSheet")
+        row_0 = sheet.row(0)
+        row_1 = sheet.row(1)
+
+        for i in range(0, 2):
+            self.assertEqual(row_0[i].value, expected_0[i])
+            self.assertEqual(row_1[i].value, expected_1[i])
+
+    def test_xlsx_row(self):
+        expected_0 = ["number", "name", "value"]
+        expected_1 = [1, "A", 3]
+        workbook = self.setup_xlsx()
+        sheet = workbook.sheet_by_name("TestSheet")
+        row_0 = sheet.row(0)
+        row_1 = sheet.row(1)
+
+        for i in range(0, 2):
+            self.assertEqual(row_0[i].value, expected_0[i])
+            self.assertEqual(row_1[i].value, expected_1[i])
+
+    def test_xls_get_cell_type(self):
+        workbook = self.setup_xls()
+        sheet = workbook.sheet_by_name("TestSheet")
+        row = sheet.row(1)
+
+        # get the type mappings
+        number_cell = row[0].NUMBER_CELL
+        text_cell = row[0].TEXT_CELL
+        empty_cell = row[0].EMPTY_CELL
+
+        # check the type mappings
+        self.assertEqual(number_cell, xlrd.XL_CELL_NUMBER)
+        self.assertEqual(text_cell, xlrd.XL_CELL_TEXT)
+        self.assertEqual(empty_cell, xlrd.XL_CELL_EMPTY)
+
+        # make sure each cell is the correct type
+        self.assertEqual(row[0]._get_cell_type(), number_cell)
+        self.assertEqual(row[1]._get_cell_type(), text_cell)
+        self.assertEqual(row[2]._get_cell_type(), number_cell)
+
+    def test_xlsx_get_cell_type(self):
+        workbook = self.setup_xlsx()
+        sheet = workbook.sheet_by_name("TestSheet")
+        row = sheet.row(1)
+
+        # get the type mappings
+        number_cell = row[0].NUMBER_CELL
+        text_cell = row[0].TEXT_CELL
+        empty_cell = row[0].EMPTY_CELL
+
+        # check the type mappings
+        self.assertEqual(number_cell, openpyxl.cell.cell.TYPE_NUMERIC)
+        self.assertEqual(text_cell, openpyxl.cell.cell.TYPE_STRING)
+        self.assertEqual(empty_cell, openpyxl.cell.cell.TYPE_NULL)
+
+        # make sure each cell is the correct type
+        self.assertEqual(row[0]._get_cell_type(), number_cell)
+        self.assertEqual(row[1]._get_cell_type(), text_cell)
+        self.assertEqual(row[2]._get_cell_type(), number_cell)
